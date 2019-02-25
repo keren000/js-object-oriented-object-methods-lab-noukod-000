16524a0a3bf08a4237d809722a7586bdf942337d
Keren-Happuc Fleurimond <keren@noukod.com>
2019-02-25 16:00:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ function BoardMember(name, homeState, training) {
     this.name = name
     this.homeState = homeState
     this.training = training
-  }
-
+  
   veto(){
     return `No, I must disagree`
   }
