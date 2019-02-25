b84124706be667bf30a2b5262e0942dc1a360977
Keren-Happuc Fleurimond <keren@noukod.com>
2019-02-25 16:05:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,77 @@
 function BoardMember(name, homeState, training) {
-    this.name = name;
-    this.homeState = homeState;
-    this.training = training;
 
-  veto(){
-    return `No, I must disagree`
+
+
+  this.name = name;
+
+
+
+  this.homeState = homeState;
+
+
+
+  this.training = training;
+
+
+
+  this.veto = function() {
+
+
+
+    return 'No, I must disagree';
+
+
+
   }
 
-  approve(){
-    return `You can do that!`
+
+
+  this.approve = function() {
+
+
+
+    return 'You can do that!'
+
+
+
   }
 
-  doCharity () {
-    return `I like to help people.`
+
+
+  this.doCharity = function() {
+
+
+
+    return 'I like to help people.'
+
+
+
   }
 
-  releasePressStatement () {
-   return `You will see great things from Scuber.`
- }
 
-  sayHi() {
+
+  this.releasePressStatement = function() {
+
+
+
+    return 'You will see great things from Scuber.'
+
+
+
+  }
+
+
+
+  this.sayHi = function() {
+
+
+
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-}
+
+
+
+  }
+
+
+
 }
